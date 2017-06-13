1870ec9fdb30d6a4a6bada3b0202a456215bbd67
gittesthln <gittesthln@gmail.com>
2017-06-13 16:42:42 +0900

    add some files

--- 10/countPrimes.php
+++ 10/countPrimes.php
@@ -1,17 +1,43 @@
 <?php
+$start = $argv[1];
+$step  = 10000000;
+$limit = 10000;
 $primes = [];
 array_push($primes,2);
-for($i=3;$i<10000; $i+=2) {
+for($i=3;$i<$limit; $i+=2) {
     $flag = true;
     for($j=0;$j<count($primes);$j++) {
-        if($i % $primes[$j] == 0) {
+				$p = $primes[$j];
+        if($i % $p == 0) {
              $flag = false;
             break;
         }
-        if($i< $primes[$j]*$primes[$j]) break;
+        if($i< $p*$p) break;
     }
     if($flag) array_push($primes, $i);
 }
-print_r($primes);
-
+if($start < $limit ) {
+  $c = count($primes);
+	$start = $limit;
+} else {
+  $c = 0;
+}
+$start = $start - $start % 2 + 1;
+$pNo = count($primes);
+$L = ($start < $step)?$step: ($start + $step);
+for($i=$start;$i<$L; $i+=2) {
+    $flag = true;
+//
+for($j=0;$j<$pNo;$j++) {
+//    for($j=0;$j<count($primes);$j++) {
+				$p = $primes[$j];
+        if($i % $p == 0) {
+             $flag = false;
+            break;
+        }
+        if($i< $p*$p) break;
+    }
+    if($flag) $c++;
+}
+print "$start $L $c\n";
 ?>

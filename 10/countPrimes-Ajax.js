a3f9e211cf12871214aa4db28282a219227a8706
gittesthln <gittesthln@gmail.com>
2017-06-16 15:15:22 +0900

    add files final

--- 10/countPrimes-Ajax.js
+++ 10/countPrimes-Ajax.js
@@ -1,27 +1,36 @@
+window.onload = function(){
+    window.Test["ajax-mode"].value = "true";
+}
 function createXMLHTTPReq( func ) {
   let xmlHttpObj = new XMLHttpRequest();
   if(xmlHttpObj) xmlHttpObj.onreadystatechange = func;
   return xmlHttpObj;
 }
 function getData() {
-    let table = document.getElementsByTagName("table")[0];
-    let request = 0, start = new Date();
+  let table = document.getElementsByTagName("table")[0];
+  let P = table.parentNode;
+  P.removeChild(table);
+  table = MKHTMLElm(P, "table",{}, {});
+  let request = 0, start = new Date();
   for(let i=0; i<10; i++) {
+    let startN = i*100000+1;
     let httpObj = createXMLHTTPReq(function(){
       if(httpObj.readyState == 4 && httpObj.status == 200) {
         let tr = MKHTMLElm(table,"tr",{}, {});
         MKHTMLElm(tr, "td", {}, {}).
-            appendChild(document.createTextNode(`${i*1000000+1}`));
+            appendChild(document.createTextNode(startN));
         MKHTMLElm(tr, "td", {}, {}).
-              appendChild(document.createTextNode(httpObj.responseText));
-          request--;
-          console.log(new Date()-start);
-          if(request <= 0) console.log("done");
+            appendChild(document.createTextNode(httpObj.responseText));
+        request--;
+        console.log(httpObj.responseText);
+        console.log(new Date()-start);
+        if(request <= 0) console.log("done");
       }
     });
-      if(httpObj) {
-          request++;
-      httpObj.open("GET",`./countPrimes.php?N=${i*1000000+1}`,true);
+    if(httpObj) {
+      request++;
+      httpObj.open("GET",`./countPrimes.php?N=${startN}`,
+                   window.Test["ajax-mode"].value == "true");
       httpObj.send(null);
     }
   }
